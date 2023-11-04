3ae4ae008859a555a937d9d30b7a0d390f98c2d3
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-04 09:13:27 +0000

    added backbone structure

--- WebApp/run.py
+++ WebApp/run.py
@@ -1,4 +1,19 @@
-from flaskblog import app
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Add a route to handle the AJAX request for music analysis
+@app.route('/analyze_music', methods=['POST'])
+def analyze_music():
+    # Get user ratings from the request
+    # Perform music analysis using your machine learning model
+    # Return the analysis results as JSON
+    # Example: return {'genre': 'Pop', 'mood': 'Happy'}
+    return {'genre': 'Pop', 'mood': 'Happy'}  # Replace this with actual results
 
 if __name__ == '__main__':
     app.run(debug=True)
