da6449ea9ab5616544b649c6ed2515fb8af4e3b5
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-12 10:46:04 +0000

    added questions blueprint

--- Testing/flaskblog/__init__.py
+++ Testing/flaskblog/__init__.py
@@ -26,8 +26,8 @@ def create_app(config_class=Config):
     from flaskblog.main.routes import main
     from flaskblog.errors.handlers import errors
     app.register_blueprint(users)
-    app.register_blueprint(posts)
-    app.register_blueprint(main)
-    app.register_blueprint(errors)
+    app.register_blueprint(questions)
+    #app.register_blueprint(main)
+    #app.register_blueprint(errors)
 
     return app
