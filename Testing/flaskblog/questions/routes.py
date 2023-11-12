a0a6d57d09f1b031b60abbde915ddcfcb26601d4
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-12 12:31:12 +0000

    need change to fit dif audio for dif questions

--- Testing/flaskblog/questions/routes.py
+++ Testing/flaskblog/questions/routes.py
@@ -10,24 +10,20 @@ def test_questions():
     user = User.query.filter_by(username=username).first()
 
     if user:
-        # Get all questions for the user -> change to one each in future
-        questions = Question.query.all()
-
         form = UserAnswerForm()
 
         if form.validate_on_submit():
-            for question in questions:
-                user_answer = UserAnswer(
-                    user_id=user.id,
-                    question_id=question.id,
-                    enjoyment_rating=getattr(form, f'question_{question.id}').data,
-                    genre_rating=getattr(form, f'question_{question.id}').data if getattr(form, f'question_{question.id}').data != 'not_sure' else None,
-                    mood_rating=getattr(form, f'question_{question.id}').data if getattr(form, f'question_{question.id}').data != 'not_sure' else None,
-                    vocal_timbre_rating=getattr(form, f'question_{question.id}').data if getattr(form, f'question_{question.id}').data != 'not_sure' else None
-                )
+            user_answer = UserAnswer(
+                user_id=user.id,
+                question_id=question.id,
+                enjoyment_rating=getattr(form, f'question_{question.id}').data,
+                genre_rating=getattr(form, f'question_{question.id}').data if getattr(form, f'question_{question.id}').data != 'not_sure' else None,
+                mood_rating=getattr(form, f'question_{question.id}').data if getattr(form, f'question_{question.id}').data != 'not_sure' else None,
+                vocal_timbre_rating=getattr(form, f'question_{question.id}').data if getattr(form, f'question_{question.id}').data != 'not_sure' else None
+            )
                 
-                db.session.add(user_answer)
-                db.session.commit()
+            db.session.add(user_answer)
+            db.session.commit()
 
             return redirect(url_for(questions.test_questions))
 
