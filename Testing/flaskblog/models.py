bde153f9ade818190e79e1f40ab21c06fdbe16e9
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-13 10:36:04 +0000

    changed outdated serializer implementation

--- Testing/flaskblog/models.py
+++ Testing/flaskblog/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import URLSafeTimedSerializer as Serializer
 from flask import current_app
 from flaskblog import db, login_manager
 from flask_login import UserMixin
