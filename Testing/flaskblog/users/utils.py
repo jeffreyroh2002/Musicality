d9148f5da96b3bc8897cb7b4b8eb0628448b9fb6
Jaehwi-Song <wotns0319@naver.com>
2023-11-16 13:20:11 +0900

    DB structure & next steps

--- Testing/flaskblog/users/utils.py
+++ Testing/flaskblog/users/utils.py
@@ -1,17 +1,19 @@
-
 import os
 import secrets
 from PIL import Image
 from flask import url_for, current_app
 from flask_mail import Message
-from flaskblog import mail
+
+# from flaskblog import mail
 
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(
+        current_app.root_path, "static/profile_pics", picture_fn
+    )
 
     output_size = (125, 125)
     i = Image.open(form_picture)
@@ -19,6 +21,7 @@ def save_picture(form_picture):
     i.save(picture_path)
 
     return picture_fn
+
 
 """
 def send_reset_email(user):
