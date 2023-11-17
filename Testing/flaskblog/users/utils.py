4149bb134508a5dad45ab372b6a649dd5d002140
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-17 09:53:04 +0000

    merge conflicts

--- Testing/flaskblog/users/utils.py
+++ Testing/flaskblog/users/utils.py
@@ -3,9 +3,7 @@ import secrets
 from PIL import Image
 from flask import url_for, current_app
 from flask_mail import Message
-
 # from flaskblog import mail
-
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
