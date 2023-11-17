f76947f6a90b4001c5c33695ef8d294bb446b23f
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-17 14:40:02 +0000

    need to fix audio path

--- Testing/flaskblog/models.py
+++ Testing/flaskblog/models.py
@@ -11,6 +11,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique = True, nullable = False)
+    name = db.Column(db.String(30), unique = False, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
     image_file = db.Column(db.String(20), nullable=False, default = 'default.jpg')
     password = db.Column(db.String(60), nullable= False)
--- Testing/flaskblog/questions/forms.py
+++ Testing/flaskblog/questions/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, SubmitField
 
 class UserAnswerForm(FlaskForm):
-    enjoyment_rating = IntegerField('How much did you enjoy this song? (Scale of 1 to 5)')
-    genre_rating = IntegerField('How much did Genre play a role in your rating above? (Scale of 1 to 5)', choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ('not_sure', 'Not sure')])
-    mood_rating = IntegerField('How much did the Mood of the song play a role in your rating above? (Scale of 1 to 5)', choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ('not_sure', 'Not sure')])
-    vocal_timbre_rating = IntegerField('How much did the Vocal Timbre of the song play a role in your rating above? (Scale of 1 to 5)', choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ('not_sure', 'Not sure')])
+    enjoyment_rating = SelectField('How much did you enjoy this song? (Scale of 1 to 5)', choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')])
+    genre_rating = SelectField('How much did Genre play a role in your rating above? (Scale of 1 to 5)', choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ('not_sure', 'Not sure')])
+    mood_rating = SelectField('How much did the Mood of the song play a role in your rating above? (Scale of 1 to 5)', choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ('not_sure', 'Not sure')])
+    vocal_timbre_rating = SelectField('How much did the Vocal Timbre of the song play a role in your rating above? (Scale of 1 to 5)', choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ('not_sure', 'Not sure')])
     submit = SubmitField('Submit')
