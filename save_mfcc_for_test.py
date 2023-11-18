6378e0d2407d57bc93ea5b4ab904b9f6fc2f579d
Jaehwi Song <wotns0319@naver.com>
2023-11-18 02:32:19 +0000

    extract mfcc from static/audio

---
+++ save_mfcc_for_test.py
@@ -0,0 +1,58 @@
+import json
+import os
+import math
+import librosa
+
+DATASET_PATH = "WebApp/static/audio"
+JSON_FILE_NAME = "audio_mfcc_data.json"
+JSON_PATH = JSON_FILE_NAME
+
+SAMPLE_RATE = 22050
+DURATION = 60  # measured in seconds for GTZAN Dataset
+SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
+
+def save_mfcc(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+
+    # data dictionary
+    data = {
+        "mapping": [],
+        "mfcc": [],
+        "labels": [],
+        "filenames": []  # Store the file names
+    }
+
+    num_samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+
+    # need the number of vectors for mfcc extraction to be equal for each segment
+    expected_num_mfcc_vectors_per_segment = math.ceil(num_samples_per_segment / hop_length)
+    
+    for file_name in os.listdir(dataset_path):
+
+        file_path = os.path.join(dataset_path, file_name)
+        signal, sr = librosa.load(file_path, sr=SAMPLE_RATE)
+
+        # process segments extracting mfcc and storing data
+        for s in range(num_segments):
+            start_sample = num_samples_per_segment * s
+            finish_sample = start_sample + num_samples_per_segment
+
+            mfcc = librosa.feature.mfcc(y=signal[start_sample:finish_sample],
+                                            sr=sr,
+                                            n_fft=n_fft,
+                                            n_mfcc=n_mfcc,
+                                            hop_length=hop_length)
+            mfcc = mfcc.T
+
+            # store mfcc for a segment if it has the expected length
+            if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+                data["mfcc"].append(mfcc.tolist())  # convert numpy array to list
+                data["labels"].append(0)
+                data["filenames"].append(file_name)  # fixed this line
+                print("{}, segment:{}".format(file_name, s))
+
+    with open(json_path, "w") as fp:
+        json.dump(data, fp, indent=4)
+
+
+if __name__ == "__main__":
+    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=20)
