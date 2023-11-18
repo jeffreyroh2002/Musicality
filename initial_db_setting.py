fb7a51f74726341b55115867b4bc3cb869d2a989
Jaehwi Song <wotns0319@naver.com>
2023-11-18 04:12:28 +0000

    adding machine learning predictions

--- initial_db_setting.py
+++ initial_db_setting.py
@@ -1,9 +1,23 @@
 from WebApp import db, create_app
 import os
+from predict_genre import predict_genre
+from predict_mood import predict_mood
+from predict_timbre import predict_timbre
+
+# path for predicting genre, mood, timbre
+genre_model_path = ""
+genre_saved_mfcc = ""
+mood_model_path = ""
+mood_saved_mfcc = ""
+timbre_model_path = ""
+timbre_saved_mfcc = ""
+test_data_path = ""
 
 app = create_app()
 
-app.app_context().push()
+genre = predict_genre(genre_model_path, test_data_path, genre_saved_mfcc)
+mood = predict_mood(mood_model_path, test_data_path, mood_saved_mfcc)
+timbre = predict_timbre(timbre_model_path, test_data_path, timbre_saved_mfcc)
 
 # Save audio files into DB
 with app.app_context():
@@ -13,6 +27,6 @@ with app.app_context():
     audio_files_dir = os.path.join(os.getcwd(), 'WebApp', 'static', 'audio')
     for file_name in os.listdir(audio_files_dir):
         file_path = os.path.join(audio_files_dir, file_name)
-        audio_file = AudioFile(audio_name=file_name, file_path=file_path, genre="None", mood="None", vocal="None")
+        audio_file = AudioFile(audio_name=file_name, file_path=file_path, genre=genre[file_name], mood=mood[file_name], vocal=timbre[file_name])
         db.session.add(audio_file)
     db.session.commit()
