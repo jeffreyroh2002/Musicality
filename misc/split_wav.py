37541af1967e699086e2b0f5bbc00125800f1983
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-19 07:33:20 +0000

    orgaanized txt files, m predict models, oter scripts

---
+++ misc/split_wav.py
@@ -0,0 +1,44 @@
+import sys
+import os
+from pydub import AudioSegment
+
+def split_wav_into_segments(input_directory, output_directory, txt_file, segment_length_ms=30000):
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_directory, exist_ok=True)
+
+    with open(txt_file, "r") as file:
+        content = file.readline()
+        while content != "":
+            content = content.strip()  # remove \n
+            data = content.split(",")
+            song_name = data[0]
+            start_time = int(data[1]) * 1000  # change ms into sec
+            end_time = int(data[2]) * 1000 
+            
+            # Load the input WAV file
+            input_path = os.path.join(input_directory, song_name)
+            audio = AudioSegment.from_wav(input_path)
+
+            segment = audio[start_time:end_time]
+
+            # Define the output file name
+            output_file = os.path.join(output_directory, song_name)
+
+            # Export the segment as a new WAV file
+            segment.export(output_file, format="wav")
+            
+            content = file.readline()
+
+        
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py input_directory output_directory")
+        sys.exit(1)
+
+    input_directory = sys.argv[1]
+    output_directory = sys.argv[2]
+    txt_file_path = sys.argv[3]
+
+    split_wav_into_segments(input_directory, output_directory, txt_file_path)
+
---
+++ misc/txt-files/stem separation_README.txt
@@ -0,0 +1,12 @@
+Songs that do not have separate Background/Vocal
+Air on the G String
+Birdhouse - Hara Noda.wav
+Lily My Dear - Bladverk Band.wav
+Royal Estate - Francis Wells
+Stellar Park - Squiid
+The Jazz Messenger - Vendla
+Trapped in a Maze - Philip Ayers
+
+
+Extra
+I'm Looking Ahead extraction not clean
