df52fba9a71426d855302b2b8b0a6bf8cba60bf1
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-26 09:27:40 +0000

    changed paths

--- WebApp/python_scripts/save_mfcc.py
+++ WebApp/python_scripts/save_mfcc.py
@@ -4,14 +4,14 @@ import math
 import librosa
 
 DATASET_PATH = os.path.join(os.getcwd(), 'WebApp', 'static', 'audio_data')
-JSON_FILE_NAME = "audio_mfcc.json"
+JSON_FILE_NAME = os.path.join(os.getcwd(), 'WebApp', 'static', 'audio_data', 'audio_full_mix_split')
 JSON_PATH = JSON_FILE_NAME
 
 SAMPLE_RATE = 22050
-DURATION = 30  # measured in seconds
+DURATION = 30  # measured in seconds for GTZAN Dataset
 SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
 
-def save_mfcc(dataset_path, json_path, sorted_file_path, n_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+def save_mfcc(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
 
     # data dictionary
     data = {
@@ -26,57 +26,33 @@ def save_mfcc(dataset_path, json_path, sorted_file_path, n_mfcc=13, n_fft=2048, 
     # need the number of vectors for mfcc extraction to be equal for each segment
     expected_num_mfcc_vectors_per_segment = math.ceil(num_samples_per_segment / hop_length)
     
-    for song in sorted_file_path :
-        data["mapping"].append(song)
-        print("\nProcessing {}".format(song))
+    for file_name in os.listdir(dataset_path):
 
-        for file_path in sorted_file_path[song]:
-            signal, sr = librosa.load(file_path, sr=SAMPLE_RATE)
+        file_path = os.path.join(dataset_path, file_name)
+        signal, sr = librosa.load(file_path, sr=SAMPLE_RATE)
 
-            # process segments extracting mfcc and storing data
-            for s in range(num_segments):
-                start_sample = num_samples_per_segment * s
-                finish_sample = start_sample + num_samples_per_segment
+        # process segments extracting mfcc and storing data
+        for s in range(num_segments):
+            start_sample = num_samples_per_segment * s
+            finish_sample = start_sample + num_samples_per_segment
 
-                mfcc = librosa.feature.mfcc(y=signal[start_sample:finish_sample],
-                                                sr=sr,
-                                                n_fft=n_fft,
-                                                n_mfcc=n_mfcc,
-                                                hop_length=hop_length)
-                mfcc = mfcc.T
+            mfcc = librosa.feature.mfcc(y=signal[start_sample:finish_sample],
+                                            sr=sr,
+                                            n_fft=n_fft,
+                                            n_mfcc=n_mfcc,
+                                            hop_length=hop_length)
+            mfcc = mfcc.T
 
-                # store mfcc for a segment if it has the expected length
-                if len(mfcc) == expected_num_mfcc_vectors_per_segment:
-                    data["mfcc"].append(mfcc.tolist())  # convert numpy array to list
-                    data["labels"].append(0)
-                    data["filenames"].append(os.path.basename(file_path))  # fixed this line
-                    print("{}, segment:{}".format(file_path.split("/")[-1], s))
+            # store mfcc for a segment if it has the expected length
+            if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+                data["mfcc"].append(mfcc.tolist())  # convert numpy array to list
+                data["labels"].append(0)
+                data["filenames"].append(file_name)  # fixed this line
+                print("{}, segment:{}".format(file_name, s))
 
     with open(json_path, "w") as fp:
         json.dump(data, fp, indent=4)
 
-def search_path(dataset_path):
-    sorted_file_paths = {}
-
-    for root, dirs, files in os.walk(dataset_path):
-        if root is not dataset_path:
-            Song_name = root.split("/")[-1]
-            sorted_file_paths[Song_name] = []
-        for file in files:
-            if file.endswith('.wav'):
-                file_path = os.path.join(root, file)
-                sorted_file_paths[Song_name].append(file_path)
-    
-    return sorted_file_paths
-
-def sort_path(path):
-    for name in path:
-        path[name] = sorted(path[name])
-    
-    print(path)
-    return path
 
 if __name__ == "__main__":
-    file_path = search_path(DATASET_PATH)
-    sorted_file_path = sort_path(file_path)
-    save_mfcc(DATASET_PATH, JSON_PATH, sorted_file_path, num_segments=10)
+    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)
