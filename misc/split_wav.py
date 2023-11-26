a0b323ab51655fa52475a90d310e991db6ae74c8
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-26 06:45:35 +0000

    avoid overwriting using counter

--- misc/split_wav.py
+++ misc/split_wav.py
@@ -1,35 +1,35 @@
 import sys
 import os
 from pydub import AudioSegment
+from datetime import datetime  # Import the datetime module
 
 def split_wav_into_segments(input_directory, output_directory, txt_file, segment_length_ms=30000):
-    # Create the output directory if it doesn't exist
     os.makedirs(output_directory, exist_ok=True)
 
     with open(txt_file, "r") as file:
         content = file.readline()
+        count = 1  # Initialize a counter for unique file names
         while content != "":
-            content = content.strip()  # remove \n
+            content = content.strip()
             data = content.split(",")
             song_name = data[0]
-            start_time = int(data[1]) * 1000  # change ms into sec
-            end_time = int(data[2]) * 1000 
-            
-            # Load the input WAV file
+            start_time = int(data[1]) * 1000
+            end_time = int(data[2]) * 1000
+
             input_path = os.path.join(input_directory, song_name)
             audio = AudioSegment.from_wav(input_path)
-
             segment = audio[start_time:end_time]
 
-            # Define the output file name
-            output_file = os.path.join(output_directory, song_name)
+            # Generate a unique output file name using a counter or timestamp
+            output_file = os.path.join(output_directory, f"{song_name}_{count}.wav")
+            # Alternatively, you can use a timestamp for uniqueness:
+            # timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
+            # output_file = os.path.join(output_directory, f"{song_name}_{timestamp}.wav")
 
-            # Export the segment as a new WAV file
             segment.export(output_file, format="wav")
             
+            count += 1  # Increment the counter
             content = file.readline()
-
-        
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -41,4 +41,3 @@ if __name__ == "__main__":
     txt_file_path = sys.argv[3]
 
     split_wav_into_segments(input_directory, output_directory, txt_file_path)
-
