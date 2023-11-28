7b77f68446a2a244e5ae225356e93d40e1cce699
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-28 15:04:30 +0000

    changes for testing

--- audioFile_checker.py
+++ audioFile_checker.py
@@ -15,19 +15,66 @@ with app.app_context():
         print(f"Audio Name: {audio_file.audio_name}")
         print(f"File Path: {audio_file.file_path}")
 
-        # Check if genre data is present and not a float
-        if audio_file.genre and not isinstance(audio_file.genre, float):
-            genre_scores = audio_file.genre
+        # Check if vocal data is present and not a float
+        if audio_file.vocal and not isinstance(audio_file.vocal, float):
+            vocal_scores = audio_file.vocal
             try:
-                genre_data = json.loads(genre_scores)
+                vocal_data = json.loads(vocal_scores)
             except json.JSONDecodeError:
-                print("Error decoding genre_data JSON.")
+                print("Error decoding vocal_data JSON.")
                 continue
 
-            print("Genre Values:")
-            for genre_name, proportion in genre_data.items():
-                print(f"  {genre_name}: {proportion}")
+            print("vocal Values:")
+            for vocal_name, proportion in vocal_data.items():
+                print(f"  {vocal_name}: {proportion}")
         else:
-            print("Genre data is not available or is in an unexpected format.")
+            print("vocal data is not available or is in an unexpected format.")
+
+        print()  # Add an empty line between entries
+
+    audio_files = db.session.query(AudioFile).all()
+
+    for audio_file in audio_files:
+        print(f"Audio Name: {audio_file.audio_name}")
+        print(f"File Path: {audio_file.file_path}")
+
+        # Check if mood data is present and not a float
+        if audio_file.mood and not isinstance(audio_file.mood, float):
+            mood_scores = audio_file.mood
+            try:
+                mood_data = json.loads(mood_scores)
+            except json.JSONDecodeError:
+                print("Error decoding mood_data JSON.")
+                continue
+
+            print("mood Values:")
+            for mood_name, proportion in mood_data.items():
+                print(f"  {mood_name}: {proportion}")
+        else:
+            print("mood data is not available or is in an unexpected format.")
+
+        print()  # Add an empty line between entries
+
+    audio_files = db.session.query(AudioFile).all()
+
+    for audio_file in audio_files:
+        print(f"Audio Name: {audio_file.audio_name}")
+        print(f"File Path: {audio_file.file_path}")
+        print(f"Vocal: {audio_file.vocal}")
+
+        # Check if vocal data is present and not a float
+        if audio_file.vocal and not isinstance(audio_file.vocal, float):
+            vocal_scores = audio_file.vocal
+            try:
+                vocal_data = json.loads(vocal_scores)
+            except json.JSONDecodeError:
+                print("Error decoding vocal_data JSON.")
+                continue
+
+            print("vocal Values:")
+            for vocal_name, proportion in vocal_data.items():
+                print(f"  {vocal_name}: {proportion}")
+        else:
+            print("vocal data is not available or is in an unexpected format.")
 
         print()  # Add an empty line between entries
