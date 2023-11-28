d642ef62358bc9805e79271383b05ab9a0230904
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-28 10:56:40 +0000

    for viewing purposes only

--- tester.py
+++ tester.py
@@ -12,8 +12,6 @@ with app.app_context():
     audio_file = db.session.query(AudioFile).first()
     print(f"Audio Name: {audio_file.audio_name}")
     print(f"File Path: {audio_file.file_path}")
-
-    # Check if genre data is present and not a float
     if audio_file.genre and not isinstance(audio_file.genre, float):
         genre_scores = audio_file.genre
         try:
@@ -21,10 +19,26 @@ with app.app_context():
         except json.JSONDecodeError:
             print("Error decoding genre_data JSON.")
 
-        print("Genre Values:")
         for genre_name, proportion in genre_data.items():
-            print(f"  {genre_name}: {proportion}")
+            print(genre_name)
+            print(proportion)
     else:
         print("Genre data is not available or is in an unexpected format.")
 
     print()  # Add an empty line between entries
+
+    # # Check if genre data is present and not a float
+    # if audio_file.genre and not isinstance(audio_file.genre, float):
+    #     genre_scores = audio_file.genre
+    #     try:
+    #         genre_data = json.loads(genre_scores)
+    #     except json.JSONDecodeError:
+    #         print("Error decoding genre_data JSON.")
+
+    #     print("Genre Values:")
+    #     for genre_name, proportion in genre_data.items():
+    #         print(f"  {genre_name}: {proportion}")
+    # else:
+    #     print("Genre data is not available or is in an unexpected format.")
+
+    # print()  # Add an empty line between entries
