cdc9ecfd55c9dceda2052b11ea4d157737350086
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-11-28 10:05:58 +0000

    fixed json float error

--- audioFile_checker.py
+++ audioFile_checker.py
@@ -8,24 +8,26 @@ app = create_app()
 
 # Assuming you are still within the application context (Musicality)
 with app.app_context():
-    # Query all AudioFile records
-    # audio_files = db.session.query(AudioFile).all()
-    audio_file = db.session.query(AudioFile).first()
 
-    # print(f"Audio Name: {audio_file.audio_name}")
-    # print(f"File Path: {audio_file.file_path}")
-    # print(f"Genre: {audio_file.genre}")
-    # print(f"Mood: {audio_file.mood}")
-    # print(f"Vocal: {audio_file.vocal}")
-    # print("\n")
+    audio_files = db.session.query(AudioFile).all()
 
-    genre_scores = audio_file.genre
-    genre_data = json.loads(genre_scores)
+    for audio_file in audio_files:
+        print(f"Audio Name: {audio_file.audio_name}")
+        print(f"File Path: {audio_file.file_path}")
 
-    for audio_name, genre_values in genre_data.items():
-        print(f"Audio Name: {audio_name}")
-        print("Genre Values:")
-        for genre, proportion in genre_values.items():
-            print(f"  {genre}: {proportion}")
+        # Check if genre data is present and not a float
+        if audio_file.genre and not isinstance(audio_file.genre, float):
+            genre_scores = audio_file.genre
+            try:
+                genre_data = json.loads(genre_scores)
+            except json.JSONDecodeError:
+                print("Error decoding genre_data JSON.")
+                continue
+
+            print("Genre Values:")
+            for genre_name, proportion in genre_data.items():
+                print(f"  {genre_name}: {proportion}")
+        else:
+            print("Genre data is not available or is in an unexpected format.")
 
         print()  # Add an empty line between entries
