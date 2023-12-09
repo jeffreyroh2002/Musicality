7b02227202b9d0390b1d332678aa5d75190eb19a
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-12-09 07:57:25 +0000

    changed to 7 options

--- WebApp/questions/forms.py
+++ WebApp/questions/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, IntegerField, SelectField, SubmitField
 from wtforms.fields import IntegerRangeField
 
 class UserAnswerForm(FlaskForm):
-    overall_rating = SelectField('How much did you enjoy this song? (Scale of 1 to 5)', choices=[(-2, 'Negative'), (-1, 'Slightly Negative'), (0, 'Average'), (1, 'Slightly Positive'), (2, 'Positive')])
-    genre_rating = SelectField('How much did Genre play a role in your rating above? (Scale of 1 to 5)', choices=[(-2, 'Negative'), (-1, 'Slightly Negative'), (0, 'Average'), (1, 'Slightly Positive'), (2, 'Positive'), ('not_sure', 'Not sure')])
-    mood_rating = SelectField('How much did the Mood of the song play a role in your rating above? (Scale of 1 to 5)', choices=[(-2, 'Negative'), (-1, 'Slightly Negative'), (0, 'Average'), (1, 'Slightly Positive'), (2, 'Positive'), ('not_sure', 'Not sure')])
-    vocal_timbre_rating = SelectField('How much did the Vocal Timbre of the song play a role in your rating above? (Scale of 1 to 5)', choices=[(-2, 'Negative'), (-1, 'Slightly Negative'), (0, 'Average'), (1, 'Slightly Positive'), (2, 'Positive'), ('not_sure', 'Not sure')])
+    overall_rating = SelectField('How much did you enjoy this song? (Scale of 1 to 7)', choices=[(-3, 'Strongly Negative'), (-2, 'Negative'), (-1, 'Slightly Negative'), (0, 'Average'), (1, 'Slightly Positive'), (2, 'Positive'), (-3, 'Strongly Positive')])
+    genre_rating = SelectField('How would you rate the Genre? (Scale of 1 to 7)', choices=[(-3, 'Strongly Negative'), (-2, 'Negative'), (-1, 'Slightly Negative'), (0, 'Average'), (1, 'Slightly Positive'), (2, 'Positive'), (-3, 'Strongly Positive')])
+    mood_rating = SelectField('How would you rate the Mood?(Scale of 1 to 7)', choices=[(-3, 'Strongly Negative'), (-2, 'Negative'), (-1, 'Slightly Negative'), (0, 'Average'), (1, 'Slightly Positive'), (2, 'Positive'), (-3, 'Strongly Positive')])
+    vocal_timbre_rating = SelectField('How would you rate the Vocals if present? (Scale of 1 to 7)', choices=[(-3, 'Strongly Negative'), (-2, 'Negative'), (-1, 'Slightly Negative'), (0, 'Average'), (1, 'Slightly Positive'), (2, 'Positive'), (-3, 'Strongly Positive')])
     submit = SubmitField('Submit')
